6721157fdb69a184e5da300d6f46333302586348
MadalinaLambru <71075396+MadalinaLambru@users.noreply.github.com>
2020-09-11 01:20:16 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+    boolean comparator = DecimalComparator.areEqualByThreeDecimalPlaces(3.1756,3.175);
+        System.out.println(comparator);
+    }
+}
